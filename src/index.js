de5edb7290269560781be289dded02e48862a4ce
rmortera <robmrtra@gmail.com>
2024-04-23 13:31:06 -0600

    update

--- src/index.js
+++ src/index.js
@@ -4,3 +4,29 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+// Sticky Navigation...
+
+const sectionHeroEl = document.querySelector(".jumbotron");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-75px",
+  }
+);
+obs.observe(sectionHeroEl);
